8be4104bd26f24c0b558a9bc0974dcaa9bd83190
Sonali301 <sonalikumari7817@gmail.com>
2025-10-21 15:52:43 +0530

    setup app, file and folder structure, routing and pages

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,9 @@
+import  { Children, ReactNode } from 'react'
+
+const RootLayout = ({children} : {children : ReactNode}) => {
+  return (
+    <div> {children} </div>
+  )
+}
+
+export default RootLayout
