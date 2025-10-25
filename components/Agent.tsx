ab62b5a5d454f5c53c3232d0c550cd7c8a546c12
Sonali301 <sonalikumari7817@gmail.com>
2025-10-25 17:24:32 +0530

    implement interview generation page ui

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -14,7 +14,7 @@ import { cn } from "@/lib/utils";
 
 
 
-enum callStatus {
+enum CallStatus {
   INACTIVE = "INACTIVE",
   CONNECTING = "CONNECTING",
   ACTIVE = "ACTIVE",
@@ -30,7 +30,15 @@ enum callStatus {
 
 const Agent = ({userName}: AgentProps ) => {
 
+    const callStatus = CallStatus.FINISHED;
     const isSpeaking = true;
+
+    const messages = [
+        "Whats your name?",
+        "My name is Sonali, nice to meet you!"
+    ];
+    const lastMessage = messages[messages.length - 1];
+    
 
   return (
 
@@ -70,21 +78,39 @@ const Agent = ({userName}: AgentProps ) => {
             </div>
 
 
+            {messages.length > 0 && (
+        <div className="transcript-border">
+          <div className="transcript">
+            <p
+              key={lastMessage}
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
+              )}
+            >
+              {lastMessage}
+            </p>
+          </div>
+        </div>
+      )}
+
+
              <div className="w-full flex justify-center">
-        {callStatus != "ACTIVE" ? (
-          <button >
-            {/* <span
+        {callStatus !== "ACTIVE" ? (
+          <button className="relative btn-call">
+            <span
               className={cn(
                 "absolute animate-ping rounded-full opacity-75",
                 callStatus !== "CONNECTING" && "hidden"
               )}
-            /> */}
+            />
 
             <span >
               {callStatus === "INACTIVE" || callStatus === "FINISHED"
                 ? "Call"
                 : ". . ."}
             </span>
+
           </button>
         ) : (
           <button className="btn-disconnect" >
